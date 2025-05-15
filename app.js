c0bbfdbafdb0cfb7ad2083595da7fb87a0a08f96
Kizzy Terra <kizzy.terra@prof.inteli.edu.br>
2025-05-15 08:47:45 -0300

    deleting old files

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const alunosRoutes = require('./routes/alunoRoutes');
+const bodyParser = require('body-parser');
+require('dotenv').config();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+
+app.use('/alunos', alunosRoutes);
+
+app.get('/', (req, res) => {
+  res.redirect('/alunos');
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
